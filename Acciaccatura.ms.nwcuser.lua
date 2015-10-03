1e4fc03da7a0288224a07487084dff589f110ba3
MikeShawaluk <mike.shawaluk@gmail.com>
2015-10-03 07:45:58 -0500

    Add "StaffSig" option, to adorn multiple grace notes

--- Acciaccatura.ms.nwcuser.lua
+++ Acciaccatura.ms.nwcuser.lua
@@ -1,24 +1,45 @@
--- Version 0.95
+-- Version 1.0
 
 --[[-----------------------------------------------------------------------------------------
-This plugin creates acciaccatura by adding a slash to a grace note (appoggiatura). The grace note receiving
-the slash must be unbeamed and eighth duration.
+This plugin creates acciaccatura by drawing a slash on the stem of a plain grace note
+(appoggiatura). The grace note receiving the slash must be unbeamed and eighth duration.
+
+If Class is set to StaffSig, then multiple grace notes will receive the slash, 
+up to the next Acciaccatura object occurrence. 
 --]]-----------------------------------------------------------------------------------------
 
+local userObjTypeName = ...
+local userObjSigName = nwc.toolbox.genSigName(userObjTypeName)
 local drawpos = nwcdraw.user
+local nextObj = nwc.ntnidx.new()
 
 local function draw_Acciaccatura(t)
-	while drawpos:find('next', 'note') and not drawpos:isGrace() do end
-	if drawpos:isGrace() and drawpos:durationBase() == 'Eighth' and not drawpos:isBeamed() then
-        local _, my = nwcdraw.getMicrons()
-	    local penWidth = my*.189
-	    nwcdraw.setPen('solid', penWidth)
-		local x, y = drawpos:xyStemTip()
-		local y1 = y - drawpos:stemDir(0)*2.4 - 1
-		nwcdraw.line(x-.3, y1, x+.7, y1+2)
+	local isStaffSig = (t.Class == 'StaffSig')
+	local w = isStaffSig and nwc.toolbox.drawStaffSigLabel(userObjSigName) or 0
+	if not nwcdraw.isDrawing() then return w end
+	if drawpos:isHidden() then return end
+	local x, y, y1
+	local _, my = nwcdraw.getMicrons()
+	local penWidth = my*.189
+	nwcdraw.setPen('solid', penWidth)
+
+	if not nextObj:find('next', 'user', userObjTypeName) then nextObj:find('last') end
+	local found = true
+	while found do
+		repeat
+			found = drawpos:find('next', 'note')
+		until drawpos:isGrace() or not found
+		
+		if found and drawpos < nextObj and drawpos:isGrace() and drawpos:durationBase() == 'Eighth' and not drawpos:isBeamed() then
+			x, y = drawpos:xyStemTip()
+			y1 = y - drawpos:stemDir(0)*2.4 - 1
+			nwcdraw.line(x-.3, y1, x+.7, y1+2)
+		end
+		if not isStaffSig then return end
 	end
 end
 
 return {
+	width = draw_Acciaccatura,
 	draw = draw_Acciaccatura
 }
