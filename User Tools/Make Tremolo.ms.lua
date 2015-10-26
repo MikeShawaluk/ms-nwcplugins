1b56b8a6b96eda7c401b363b403c734aaca32547
MikeShawaluk <mike.shawaluk@gmail.com>
2015-10-26 18:25:30 -0500

    Change setlevel to 2, allow for dotted chords, etc.

--- User Tools/Make Tremolo.ms.lua
+++ User Tools/Make Tremolo.ms.lua
@@ -1,5 +1,5 @@
 --[[-------------------------------------------------------------------------
-Version 0.1
+Version 0.2
 
 This NWC user tool can be used to convert two notes into a tremolo, or to "clean up"
 a rest chord so its note and rest durations are correct for tremolos.
@@ -8,15 +8,13 @@ It does the following:
 
 1. Convert each non-split-voice note/chord to a rest chord, whose rest duration is 
    1/2 of the chord's duration. The rest will be hidden and the chord will be muted.
+   Any dot on the source chord will be preserved in the rest chord.
 2. For each pair of converted chords, insert a Tremolo.ms object before the second one.
-3. For any encountered rest chords, adjust the note duration so it is consistent with
-   the rest duration for Tremolo usage.
 
 $NWCUT$CONFIG: ClipText $
 --]]-------------------------------------------------------------------------
 
--- we want to work with basic text for most lines
-nwcut.setlevel(1)
+nwcut.setlevel(2)
 local noteDurBase = nwc.txt.NoteDurBase --{'Whole', 'Half', '4th', '8th', '16th', '32nd', '64th'}
 local noteDurBaseRev = {}
 
@@ -26,52 +24,55 @@ end
 
 local changeCount, addCount = 0, 0
 local firstNote = true
-local dur, pos, dur2, opts, stemDir
+local dur, pos, dur2, opts, stemDir, dot, duration
+
+local function warnline(string, value)
+	local s1, s2 = value == 0 and 'No' or tostring(value), value == 1 and '' or 's'
+	nwcut.warn(string.format(string .. '\n', s1, s2))
+end
+
+local function parseDur(durTable)
+	local dur, dot
+	for v in pairs(durTable) do
+		dot = dot or (v == 'Dotted' or v == 'DblDotted') and v
+		dur = dur or noteDurBaseRev[v] and v
+	end
+	return dur, dot
+end
 
 for item in nwcut.items() do
 	if item:IsFake() then
 		-- don't process these
 	elseif item:ContainsNotes() then
 		if item:Is('RestChord') then
-			-- for existing RestChords, adjust the note duration to match the rest
-			local newItem = nwcItem.new('|RestChord')
-			dur = item:Get('Dur')
-			pos = item:Get('Pos2')
-			dur2 = item:Get('Dur2')
-			opts = item:Get('Opts')
-
-			if dur == 'Whole' then
-				dur = 'Half'
-			else
-				dur2 = dur == '16th' and '4th' or noteDurBase[noteDurBaseRev[dur]-1]
-			end
-
-			newItem:Provide('Dur2', dur2)
-			newItem:Provide('Dur', dur)
-			newItem:Provide('Opts', opts)
-			newItem:Provide('Pos2', pos)
-			changeCount = changeCount + 1
-			item = newItem
+			-- skip rest chords
 		else
 			-- for regular notes/chords, convert them to a muted RestChord with hidden rest
 			local newItem = nwcItem.new('|RestChord')
 			dur = item:Get('Dur')
 			pos = item:Get('Pos')
 			dur2 = item:Get('Dur2')
-			stemDir = 'Down'
+			opts = item:Get('Opts')
+			stemDir = item:Get('Opts', 'Stem') or 'Up'
+			stemDir = stemDir == 'Up' and 'Down' or 'Up'
 
 			if not dur2 then -- don't convert split voice chords
-				local newDur = noteDurBase[noteDurBaseRev[dur]+1]
-				if dur == '8th' then dur = '4th' end
-				newItem:Provide('Dur2', dur)
+				duration, dot = parseDur(dur)
+				local newDur = noteDurBase[noteDurBaseRev[duration]+1] or '64th'
+				if duration == '8th' then duration = '4th' end
+				newItem:Provide('Dur2', duration)
+				if dot then newItem:Provide('Dur2')[dot] = '' end
 				newItem:Provide('Dur', newDur)
-				newItem:Provide('Opts', string.format('Stem=%s,HideRest,Muted', stemDir))
+				newItem:Provide('Opts', opts)
+				newItem:Provide('Opts').Stem = stemDir
+				newItem:Provide('Opts').HideRest = ''
+				newItem:Provide('Opts').Muted = ''
 				newItem:Provide('Pos2', pos)
 				item = newItem
 				changeCount = changeCount + 1
 				firstNote = not firstNote
 				if firstNote then
-					nwcut.writeline(nwcItem.new('|User|Tremolo.ms|Pos:0'))
+					nwcut.writeline(nwcItem.new('|User|Tremolo.ms|Pos:0' .. (dot and '|TripletPlayback:Y' or '')))
 					addCount = addCount + 1
 				end
 			end
@@ -81,8 +82,5 @@ for item in nwcut.items() do
 	nwcut.writeline(item)
 end
 
-if changeCount + addCount == 0 then
-	nwcut.warn('No objects were added or updated\n')
-else
-	nwcut.warn(string.format('%d chords modified, %d Tremolo.ms objects added.\n', changeCount, addCount) )
-end
+warnline('%s chord%s will be converted.', changeCount)
+warnline('%s Tremolo.ms object%s will be added.', addCount)
