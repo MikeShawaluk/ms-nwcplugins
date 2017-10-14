0b84be905a6763280fb4d0c0e3100f28ec920401
MikeShawaluk <mike.shawaluk@gmail.com>
2017-10-14 05:56:10 -0500

    Acciaccatura 1.21 Add playback

--- Acciaccatura.ms.nwcuser.lua
+++ Acciaccatura.ms.nwcuser.lua
@@ -1,11 +1,18 @@
--- Version 1.1
+-- Version 1.21
 
 --[[-----------------------------------------------------------------------------------------
 This plugin creates acciaccatura by drawing a slash on the stem of a plain grace note
-(appoggiatura). The grace note receiving the slash must be unbeamed and eighth duration.
+(appoggiatura). The grace note receiving the slash must be unbeamed and eighth duration. The
+plugin will optionally play the acciaccatura before the beat, when the grace note is muted
+and a nonzero rate is specified.
 
-If Class is set to StaffSig, then multiple grace notes will receive the slash, 
-up to the next Acciaccatura object occurrence. 
+If Class is set to StaffSig, then multiple grace notes will be processed, 
+up to the next Acciaccatura object occurrence.
+
+@Rate
+The playback rate for the acciaccatura, expressed as the number of notes per whole note
+duration. For example, 64 would correspond to a 1/64 note. A value of 0 disables playback,
+which is the default.
 --]]-----------------------------------------------------------------------------------------
 
 local userObjTypeName = ...
@@ -13,7 +20,11 @@ local userObjSigName = nwc.toolbox.genSigName(userObjTypeName)
 local drawpos = nwcdraw.user
 local nextObj = nwc.ntnidx.new()
 
-local function draw_Acciaccatura(t)
+local _spec = {
+	{ id='Rate', label='Rate', type='float', default=0, min=0, max=128, step=1 },
+}
+
+local function _draw(t)
 	local isStaffSig = (t.Class == 'StaffSig')
 	local w = isStaffSig and nwc.toolbox.drawStaffSigLabel(userObjSigName) or 0
 	if not nwcdraw.isDrawing() then return w end
@@ -39,7 +50,31 @@ local function draw_Acciaccatura(t)
 	end
 end
 
+local function _play(t)
+	local isStaffSig = (t.Class == 'StaffSig')
+	local play = nwcplay.user
+	if t.Rate == 0 then return end
+	if not nextObj:find('next', 'user', userObjTypeName) then nextObj:find('last') end
+	local found = true
+	while found do
+		repeat
+			found = play:find('next', 'note')
+		until play:isGrace() or not found
+		if found and play < nextObj and play:isGrace() and play:durationBase() == 'Eighth' and not play:isBeamed() and play:isMute() then
+			local noteCount = play:noteCount()
+			local duration = (4 * nwcplay.PPQ / t.Rate)
+			local start = play:sppOffset() - duration
+			for i = 1, noteCount do
+				nwcplay.note(start, duration, nwcplay.getNoteNumber(play:notePitchPos(i)))
+			end
+		end
+		found = found and isStaffSig
+	end
+end
+
 return {
-	width = draw_Acciaccatura,
-	draw = draw_Acciaccatura
+	spec = _spec,
+	width = _draw,
+	draw = _draw,
+	play = _play,
 }
